c4ce59bc3f25e966f63f8069fa47b05d95a3ee17
Avinash Sahani <avinash.sahani@astera.com>
2023-04-16 05:05:19 +0500

    🎨(ExpenseItems.css,ExpenseItem.js) : Added parameters to pass data to Expense Item

--- Section 3 (React Basics)/react-basics/src/components/ExpenseItem.js
+++ Section 3 (React Basics)/react-basics/src/components/ExpenseItem.js
@@ -1,6 +1,21 @@
+import "./ExpenseItem.css";
 
-
-function ExpenseItem(){
-    return <h2> Expense item!</h2>
+function ExpenseItem(props) {
+    const date = props.date;
+    const month = date.toLocaleString('en-US', {month: 'long'});
+    const day = date.toLocaleString('en-US', {day: '2-digit'});
+    const year = date.getFullYear();
+    return (<div className="expense-item">
+        <div>
+            <div>{month}</div>
+            <div>{year}</div>
+            <div>{day}</div>
+        </div>
+        <div className="expense-item__description">
+            <h2>{props.title}</h2>
+            <div className="expense-item__price">${props.amount}</div>
+        </div>
+    </div>)
 }
+
 export default ExpenseItem;
