a49a58b9697122c15dc12898609546b57cefe4f5
Avinash Sahani <avinash.sahani@astera.com>
2023-04-16 06:28:35 +0500

    🎨(ExpenseDate,ExpenseItem) : Seperated Expense Date Component

--- Section 3 (React Basics)/react-basics/src/components/ExpenseItem.js
+++ Section 3 (React Basics)/react-basics/src/components/ExpenseItem.js
@@ -1,17 +1,9 @@
 import "./ExpenseItem.css";
-
+import  ExpenseDate from "./ExpenseDate";
 function ExpenseItem(props) {
-    const date = props.date;
-    const month = date.toLocaleString('en-US', {month: 'long'});
-    const day = date.toLocaleString('en-US', {day: '2-digit'});
-    const year = date.getFullYear();
     return (<div className="expense-item">
-        <div>
-            <div>{month}</div>
-            <div>{year}</div>
-            <div>{day}</div>
-        </div>
-        <div className="expense-item__description">
+         <div className="expense-item__description">
+            <ExpenseDate date={props.date}></ExpenseDate>
             <h2>{props.title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
