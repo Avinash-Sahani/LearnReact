ac5678c8f57a6f744be259aca8e4c7ebfa72bd53
Avinash Sahani <avinash.sahani@astera.com>
2023-04-22 04:54:09 +0500

    🎨(ExpenseItem.js) :  Learned UseState Function

--- Section 3 (React Basics)/react-basics/src/components/Expenses/ExpenseItem.js
+++ Section 3 (React Basics)/react-basics/src/components/Expenses/ExpenseItem.js
@@ -1,14 +1,21 @@
   import "./ExpenseItem.css";
 import  ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
-  import React  from "react";
+  import React, {useState} from "react";
 function ExpenseItem(props) {
+
+
+    const [title,setTitle] = useState(props.title)
+    const clickHanlder = ()=>{
+        setTitle("Updated")
+    }
     return (<Card className="expense-item">
          <div className="expense-item__description">
             <ExpenseDate date={props.date}></ExpenseDate>
-            <h2>{props.title}</h2>
+            <h2>{title}</h2>
             <div className="expense-item__price">${props.amount}</div>
         </div>
+        <button onClick={clickHanlder}> Click Me</button>
     </Card>)
 }
 
